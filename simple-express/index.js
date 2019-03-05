0fa28980be4cd703f859628f62f577ef22d1f259
Sean Fitzgerald <sean.fitzgerald@aftermarketanalytics.com>
2019-03-04 20:49:29 -0700

    added shopping list route

--- simple-express/index.js
+++ simple-express/index.js
@@ -4,4 +4,14 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/shopping-list', (req, res) =>{
+  const groceries = [
+    'Milk',
+    'Eggs',
+    'Bread',
+    'Butter',
+  ];
+  res.json(groceries);
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
